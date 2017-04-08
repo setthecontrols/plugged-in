5fb05230e49faefdf8ff8111f2c3361bba9b7692
Brandon Garibaldi <bmg.oak@gmail.com>
2017-04-07 17:34:39 -0700

    adds user profile fields to user model.  adds better_errors gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407195224) do
+ActiveRecord::Schema.define(version: 20170408003129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,19 @@ ActiveRecord::Schema.define(version: 20170407195224) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "location"
+    t.string   "genre"
+    t.text     "music_links"
+    t.string   "band_status"
+    t.string   "slogan"
+    t.text     "bio"
+    t.text     "experience"
+    t.string   "instruments"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
