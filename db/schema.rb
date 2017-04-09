0c022f11a17b8ab43a42b7405818a88568a36add
Shaun Yee <yeezy@Shauns-MacBook-Pro.local>
2017-04-08 20:16:07 -0700

    styles index page and layout

--- db/schema.rb
+++ db/schema.rb
@@ -32,14 +32,11 @@ ActiveRecord::Schema.define(version: 20170408200343) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.index ["boardcategory_id"], name: "index_boardposts_on_boardcategory_id", using: :btree
+    t.index ["user_id"], name: "index_boardposts_on_user_id", using: :btree
   end
 
   create_table "conversations", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "searches", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
