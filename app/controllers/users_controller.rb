65b4357601344e28d1bc44924ca47bc0b2b873c0
Joe Moorhouse <jbmoorhouse1@gmail.com>
2017-04-09 02:57:31 -0700

    changing user password still not functions, all other user information can be updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     # @user = User.find(current_user.id)
-
   end
 
   def edit
     @user = current_user
+  end
+
+  def update_password
+    @user = User.find(current_user.id)
+      if @user.update(password_params)
+        # Sign in the user by passing validation in case their password changed
+        bypass_sign_in(@user)
+        redirect_to root_path
+      else
+        render "edit"
+      end
   end
 
   def update
@@ -26,5 +36,9 @@ class UsersController < ApplicationController
   end
   def update_user_params
     params.require(:user).permit(:first_name, :last_name, :email, :username, :bio, :location, :band_status, :experience, :instruments, :slogan)
+  end
+  def password_params
+    # NOTE: Using `strong_parameters` gem
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
