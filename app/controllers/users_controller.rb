31d46e3df751bcc842f6e7346cd206a99358f8ee
Brandon Garibaldi <bmg.oak@gmail.com>
2017-04-08 18:50:02 -0700

    removes yeild and html tags from landing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   def show
   end
 
+  
+
+  private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
    end
