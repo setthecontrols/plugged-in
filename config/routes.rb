51606784cc5548422100a8c5c9a2a36fc29c16cd
Dylan Cordoni <email": null>
2017-04-12 20:39:18 -0700

    Adds: method to find new connections

--- config/routes.rb
+++ config/routes.rb
@@ -20,11 +20,10 @@ Rails.application.routes.draw do
   resources :conversations
   resources :users do
     resources :user_conversations, shallow: true
-  end
-
-  resources :user do
     resources :connections, shallow: true
   end
+
+
 
 # post "/users/:user_id/connections" => "connections#create", :as => :create_connection
   root to: "pages#home"
