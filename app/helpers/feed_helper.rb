5a4ce75c781b6f78ab9e52dfacc6b3329bd33b36
Brandon Garibaldi <bmg.oak@gmail.com>
2017-04-12 22:11:30 -0700

    adds custom route and user model methods to collect user connections data for activity feed

---
+++ app/helpers/feed_helper.rb
@@ -0,0 +1,8 @@
+module FeedHelper
+
+
+
+
+
+
+end
--- app/models/useraudiofile.rb
+++ app/models/useraudiofile.rb
@@ -1,7 +1,8 @@
 class Useraudiofile < ApplicationRecord
   has_attached_file :media,
                     :url => "/assets/:class/:id/:attachment/:style.:extension",
-                    :path => ":rails_root/public/assets/:class/:id/:attachment/:style.:extension"
+                    :path => ":rails_root/public/audios"
+                    # :path => ":rails_root/public/assets/:class/:id/:attachment/:style.:extension"
   validates_attachment_content_type :media, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
 
 
