cb88a95cebeb3a709601192fd62dc1dd356eb5ef
Dylan Cordoni <email": null>
2017-04-13 18:01:06 -0700

    Fixes: search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -13,5 +13,20 @@ class SearchesController < ApplicationController
     @model = params[:model].downcase
     @results = search_specific(@keyword, @model)
     @found = any_results_specific?(@results)
+    if @model == "users-all"
+      @displayed_model = "Musicians"
+    elsif @model == "posts"
+      @displayed_model = "Postings"
+    elsif @model == "categories"
+      @displayed_model = "Listing Categories"
+    elsif @model == "users-location"
+      @displayed_model = "Location"
+    elsif @model == "users-genre"
+      @displayed_model = "Genres"
+    elsif @model == "users-instruments"
+      @displayed_model = "Instruments"
+    elsif @model == "users-name"
+      @displayed_model = "Musicians by Name"
+    end
   end
 end
